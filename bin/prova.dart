6fcbc1431a56532fa23500b0d0ef03ad9b048c8f
devfelpSs <devfelps@outlook.com>
2023-02-13 14:19:16 -0300

    first commit

--- bin/prova.dart
+++ bin/prova.dart
@@ -1,10 +1,18 @@
 void main(List<String> args) {
-  var prova1 = 10;
+  var prova1 = 11;
   var prova2 = 5;
   var media = (prova1 + prova2) / 2;
   if (media >= 7) {
-    print("o aluno passou com a nota: " + media.toString());
+    print("o aluno passou com a nota: $media");
+  } else if (media >= 5){
+    print("O aluno está de recuperação com a nota: $media");
   } else {
-    print("O aluno reprovou com a nota: " + media.toString());
+    print("O aluno reprovou com a nota: $media");
   }
 }
+
+
+/* Nesta aula é mostrada a utilização do if, else e else if aonde pode ser utilizado em N situações
+O sinal de $ nas linhas 6 e 7 na palavra media é para dizer que ali pode-se interpretar 
+variaveis não strings em variaveis strings.
+*/
